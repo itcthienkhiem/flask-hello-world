cbefe32796946abb84d8227cc8964d1ef5457ffd
khiem.nthien <itc.thienkhiem@gmail.com>
2023-07-23 21:13:44 +0700

    first commit

--- api/index.py
+++ api/index.py
@@ -1,11 +1,44 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 from flask import Flask
+from bardapi import Bard
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/bardapi/speech')
+def speech():
+    bard = Bard(token='YgjdenmnVllP0049BbUVYL7_U8yPtf34t76GZNAqfrisYEUEkA4so6W30BHZpBPyFdGmhQ.')
+    audio = bard.speech("Xin chào, bạn tên là gì?", lang='vi-VN')  # Get bytes audio object.
+
+    # Save audio object.
+    with open('bard_response.mp3', 'wb') as f:
+        f.write(audio)
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/bardapi/<question>',methods=('POST',))
+def get_answer(question):
+
+    #question = "Quán massage chổ nào gần đây？";
+    bard = Bard(token='YgjdenmnVllP0049BbUVYL7_U8yPtf34t76GZNAqfrisYEUEkA4so6W30BHZpBPyFdGmhQ.', language='vietnamese',
+                timeout=30)
+    res = bard.get_answer(question)
+    #print(res['content'])
+    return res['content']
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
